1f05a2441076460cd8d69d58657accecb81fc3f6
vivaxy <xyxuye2007@126.com>
2016-01-06 11:53:08 +0800

    add spacing

--- index.js
+++ index.js
@@ -13,18 +13,23 @@
 // MIT license
 
 (function () {
+
     var frameInterval = 16;
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
 
     for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+
         window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
         window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
             window[vendors[x] + 'CancelRequestAnimationFrame'];
+
     }
 
     if (!window.requestAnimationFrame) {
+
         window.requestAnimationFrame = function (callback) {
+
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, frameInterval - (currTime - lastTime));
             var id = window.setTimeout(function () {
@@ -34,12 +39,17 @@
 
             lastTime = currTime + timeToCall;
             return id;
+
         };
+
     }
 
     if (!window.cancelAnimationFrame) {
+
         window.cancelAnimationFrame = function (id) {
             clearTimeout(id);
         };
+
     }
+
 }());
